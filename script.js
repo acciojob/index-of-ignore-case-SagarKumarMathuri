6c2f1bb5a8e02bc1d61b36c7ff9093fa7bb1f9d5
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-21 10:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// function indexOfIgnoreCase(s1, s2) {
+//   // write your code here
+	
+// }
+
+// // Please do not change the code below
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  if (s2 === "") return 0; // edge case
+  
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
